c080cf5d7d63c156b1215f0c639d941f4fbbcb12
Dmitry Fyodorov <fedorov.ss-4554@yandex.ru>
2024-01-08 21:21:20 +0300

    убрал ненужный код

--- main.py
+++ main.py
@@ -11,13 +11,6 @@ if __name__ == "__main__":
     game = Engine()
     screen = game.window(WIDTH, HEIGHT, TITLE, DIR_IMG+"/fox.ico", DIR_IMG+"/background/bg2.png")
     w, h = game.get_size()
-    #############################################
-    # sprites = loading_sprites(SPRITES["fox"])
-    # fox = HeroFox(screen, w, h, sprites)
-    # sprites = loading_sprites(SPRITES["bush"])
-    # bush0 = Stationary(screen, w*0, h, sprites)
-    #############################################
     fox = HeroFox(screen, w, h, loading_sprites(SPRITES["fox"]))
     bush0 = Stationary(screen, w*0, h, loading_sprites(SPRITES["bush"]))
-    #############################################
     game.loop(fox=fox, bush=bush0)
